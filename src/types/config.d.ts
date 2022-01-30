cfc4546b53d7e584fca215e8026c9303a8d83abb
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2022-01-30 11:04:37 +0100

    Add missing libs

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -4,9 +4,4 @@ declare module 'react-native-config' {
 
   // API
   export const API_URL_BASE: string
-  export const GOOGLE_WEB_CLIENT_ID: string
-  export const ENABLE_EXTERNAL_LOGIN_PROVIDERS: 'false' | 'true'
-  export const GOOGLE_PLACE_API_KEY: string
-  export const OAUTH_FACEBOOK_ID: string
-  export const OAUTH_FACEBOOK_SECRET: string
 }
